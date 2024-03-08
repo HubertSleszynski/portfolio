d660f0c6b6bd12b57fdf42a5b4f3cedee82797dc
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-03-08 17:00:17 +0100

    added another project

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,12 +1,12 @@
 import toDoList from "../../assets/to-do-list.png";
 import logicalGames from "../../assets/logical-games.png";
-import work3 from "../../assets/work3.jpg";
-import work4 from "../../assets/work4.jpg";
+import hrsLibrary from "../../assets/hrs.library.png";
+import work1 from "../../assets/work1.jpg";
 
 export const projectsData = [
   {
     id: 1,
-    image: work3,
+    image: work1,
     title: "E-commerce shop UI",
     technologies: "",
     description:
@@ -15,16 +15,18 @@ export const projectsData = [
   },
   {
     id: 2,
-    image: work3,
+    image: hrsLibrary,
     title: "Book searching app",
-    technologies: "",
+    technologies: "HTML CSS JavaScript React API",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni voluptas voluptates modi asperiores facilis dignissimos rerum quis at, sit ullam velit dolorum harum, ut dolores nemo! Animi possimus architecto fugiat.",
+      "This web application is a minimalist tool for quickly accessing book information. The straightforward search allows you to input the title, and instantly receive essential details, letting you focus solely on what matters most - the content of the books. Fast, convenient, and without any unnecessary clutter.",
     category: "Frontend",
+    live: "https://hubertsleszynski.github.io/library_api_v2/",
+    github: "https://github.com/HubertSleszynski/library_api_v2",
   },
   {
     id: 3,
-    image: work3,
+    image: work1,
     title: "Job searching app",
     technologies: "HTML CSS TAILWIND JS REACT NODE MONGODB EXPRESS ",
     description:
@@ -33,7 +35,7 @@ export const projectsData = [
   },
   {
     id: 4,
-    image: work4,
+    image: work1,
     title: "Pizza Shop",
     technologies: "HTML CSS JS REACT NODE MONGODB",
     description:
@@ -42,24 +44,25 @@ export const projectsData = [
   },
   {
     id: 5,
-    image: logicalGames,
-    title: "Logical games",
-    technologies: "HTML CSS JS",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, sunt architecto! Harum provident assumenda corrupti est, nam magnam consequatur quam quisquam inventore ducimus, ex voluptatibus impedit pariatur modi quia rem!",
-    category: "Basics",
-    live: "https://hubertsleszynski.github.io/four-logical-games/",
-    github: "https://github.com/HubertSleszynski/four-logical-games",
-  },
-  {
-    id: 6,
     image: toDoList,
     title: "To Do List",
-    technologies: "HTML CSS JS REACT",
-    description: "Could a resume be completed without it?",
+    technologies: "HTML CSS JavaScript React",
+    description:
+      "The to-do list application is a tool designed for efficient task management. With its dynamic user interface, you can easily add, remove, and mark tasks as completed. It ensures smooth and instant updates, allowing you to track progress without unnecessary delays. Is it possible to complete a portfolio without this?",
     category: "Basics",
     live: "https://hubertsleszynski.github.io/to-do-list/",
     github: "https://github.com/HubertSleszynski/to-do-list",
+  },
+  {
+    id: 6,
+    image: logicalGames,
+    title: "Logical games",
+    technologies: "HTML CSS JavaScript",
+    description:
+      "The app features a central platform with four buttons providing access to various logic games. Here, you'll find an exciting Memory game, a mind-engaging Sudoku, a Puzzle Slider challenge, and the classic Tic Tac Toe. Simple navigation and an aesthetic design ensure a pleasant user experience, allowing easy transitions between different logic games.",
+    category: "Basics",
+    live: "https://hubertsleszynski.github.io/four-logical-games/",
+    github: "https://github.com/HubertSleszynski/four-logical-games",
   },
 ];
 
